f1cb993a44a1490124a1d9125f155515d501ee4d
Alfred  Chibuike Theophane <72687582+chibyk71@users.noreply.github.com>
2026-09-04 15:05:33 +0100

    fix: restore correct AddLinkHeaders middleware namespace in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(
             append: [
                 \App\Http\Middleware\HandleInertiaRequests::class,
-                \Illuminate\Middleware\AddLinkHeadersForPreloadedAssets::class,
+                \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
                 \App\Http\Middleware\EnsureCurrentSession::class
             ],
             prepend: [
